2d349c3dbb5046ff549700f36f9b5c445dbdbe1b
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-04-23 23:13:25 -0600

    export dbhelperstypes types

--- src/lib/types/db/helpers.ts
+++ src/lib/types/db/helpers.ts
@@ -4,38 +4,38 @@ import { ReturnTypes } from '../returns';
 
 export namespace DBHelpersTypes {
   // Model.exists()
-  type ExistsDataType<DocumentType> = Pick<
+  export type ExistsDataType<DocumentType> = Pick<
     Document<DocumentType, any, any>,
     '_id'
   > | null;
-  type ExistsFunction<DocumentType> = (
+  export type ExistsFunction<DocumentType> = (
     filter: FilterQuery<DocumentType>
   ) => ReturnTypes.AsyncTuple<
     ExistsDataType<Document>,
     InstanceType<typeof DatabaseOperationError>
   >;
   // Model.save()
-  type SaveFunction<DocumentType> = (
+  export type SaveFunction<DocumentType> = (
     document: DocumentType
   ) => ReturnTypes.AsyncTuple<
     DocumentType,
     InstanceType<typeof DatabaseOperationError>
   >;
   // Model.findOneById()
-  type FindByIdDataType<DocumentType> =
+  export type FindByIdDataType<DocumentType> =
     | (DocumentType & {
         _id: any;
       })
     | null;
-  type FindByIdFunction<DocumentType> = (
+  export type FindByIdFunction<DocumentType> = (
     id: string
   ) => ReturnTypes.AsyncTuple<
     FindByIdDataType<DocumentType>,
     InstanceType<typeof DatabaseOperationError>
   >;
   // Model.findOne()
-  type FindOneDataType<DocumentType> = FindByIdDataType<DocumentType>;
-  type FindOneFunction<DocumentType> = (
+  export type FindOneDataType<DocumentType> = FindByIdDataType<DocumentType>;
+  export type FindOneFunction<DocumentType> = (
     filter: FilterQuery<DocumentType>
   ) => ReturnTypes.AsyncTuple<
     FindOneDataType<DocumentType>,

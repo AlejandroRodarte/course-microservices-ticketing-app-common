6692eea997fa33a48d83095fbe75bba1c26bae1f
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-05-15 01:41:42 -0600

    fix small natserror typo

--- src/lib/objects/nats/publisher.ts
+++ src/lib/objects/nats/publisher.ts
@@ -1,6 +1,6 @@
 import nats from 'node-nats-streaming';
 import { NatsTypes } from '../../types/nats';
-import NatsPublisherError from '../errors/nats-error';
+import NatsError from '../errors/nats-error';
 
 export default abstract class Publisher<E extends NatsTypes.Event> {
   abstract subject: E['subject'];
@@ -11,10 +11,10 @@ export default abstract class Publisher<E extends NatsTypes.Event> {
     this.client = client;
   }
 
-  async publish(data: E['data']): Promise<NatsPublisherError | undefined> {
-    return new Promise<NatsPublisherError | undefined>((resolve, reject) => {
+  async publish(data: E['data']): Promise<NatsError | undefined> {
+    return new Promise<NatsError | undefined>((resolve, reject) => {
       this.client.publish(this.subject, JSON.stringify(data), (err, guid) => {
-        if (err) resolve(new NatsPublisherError(err.message));
+        if (err) resolve(new NatsError(err.message));
         else resolve(undefined);
       });
     });
